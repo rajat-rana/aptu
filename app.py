718f612a8ab51450b425acbdac97ec98a878e4a9
Rajat agrawal <rajat.agrawal193@gmail.com>
2020-07-23 22:50:45 +0530

    few changes in server

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from applications import home
 import os
 
 
-server = Flask(__name__)
-app = home.create_app(server)
-@server.route('/')
-def hello():
+# server = Flask(__name__)
+app = home.create_app(Flask(__name__))
+server = app.server
+# @server.route('/')
+if __name__ == '__main__':
     app.run_server(debug=True)
