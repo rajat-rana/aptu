50ff2f723d39165e4daf4a6fafad9fdb4a57457b
Rajat agrawal <rajat.agrawal193@gmail.com>
2020-07-24 00:05:21 +0530

    applications added

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from applications import home
+from applications import app2
+
 #app = Flask(__name__)
 
 import os
 
 
 server = Flask(__name__)
-app = home.create_app(Flask(__name__))
-# server = app.server
-@server.route('/')
-def home():
-    app.run_server(debug=True)
+app = home.create_app(server, '/app/')
+app_2 = app2.create_app(server, '/app2/')
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+
+@server.route('/')
+def hello():
+    return " Hello World. change routes  to '/app' and 'app2' to go to apps"
+
+
+# @server.route('/app2')
+# def hello_app():
+#     return "Hello App"
---
+++ applications/app2.py
@@ -0,0 +1,37 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+
+def create_app(server, path):
+
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+    app = dash.Dash(
+        __name__, external_stylesheets=external_stylesheets, server=server, routes_pathname_prefix=path)
+
+    # assume you have a "long-form" data frame
+    # see https://plotly.com/python/px-arguments/ for more options
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+    app.layout = html.Div(children=[
+        html.H1(children='Hello Dash'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(
+            id='example-graph',
+            figure=fig
+        )
+    ])
+    return app
